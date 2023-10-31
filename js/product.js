877776c3bfcde884f5ed8d15c5c201b08f53f7f0
Volatuss <95890719+Volatuss@users.noreply.github.com>
2023-10-31 13:00:04 -0500

    Listing Improvements and Product Pages
    Improved upon the listings by adding images and sales/discounts. "View Product" button now functions and redirects to a product page which is currently WIP.

---
+++ js/product.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {    
+    // Get the product ID from the URL query parameter
+	const urlParams = new URLSearchParams(window.location.search);
+	const productId = urlParams.get("id");
+
+	// Fetch the product data from the database Once we have a db
+	// Temporary data here for testing
+	const data = [
+		{
+			"id": "1",
+			"name": "Windows 10 Home Key",
+			"price": "$20.00",
+			"salePrice": "",
+			"image": "winKeyImg.jpg",
+			"tags": ["microsoft product", "operating system"]
+		},
+		{
+			"id": "2",
+			"name": "Baldurs Gate 3 (PC) Key",
+			"price": "$34.00",
+			"salePrice": "",
+			"image": "bg3KeyImg.jpg",
+			"tags": ["game", "rpg", "open world"]
+			
+		},
+		{
+			"id": "3",
+			"name": "Starfield (PC) Key",
+			"price": "$38.00",
+			"salePrice": "$29.99",
+			"image": "stfKeyImg.jpg",
+			"tags": ["game", "rpg", "open world", "pc", "sale"]
+			
+		},
+	];
+
+    
+    if (productId) {
+        fetchProductDetails(productId);
+    }
+
+    function fetchProductDetails(id) {
+
+        const product = data.find((item) => item.id === id);
+
+        if (product) {
+            // Update the product details on the product.html page
+            updateProductDetailsPage(product);
+        } else {
+            // Handle the case when the product is not found (e.g., display an error message)
+            console.log("Product not found");
+        }
+    }
+
+    function updateProductDetailsPage(product) {
+        // Update the product banner image, name, description, and price
+        document.getElementById("product-banner").src = `../assets/${product.image}`;
+        document.getElementById("product-name").textContent = product.name;
+        document.getElementById("product-description").textContent = product.description;
+        document.getElementById("product-price").textContent = product.price;
+    }
+});
--- js/scripts.js
+++ js/scripts.js
@@ -8,55 +8,85 @@ document.addEventListener("DOMContentLoaded", function () {
 	// Get Reference to the Product Container, which will hold all the items
 	const productContainer = document.getElementById("product-container");
 	const filterSelect = document.getElementById("filter-select");
+	// Get the product ID from the URL query parameter
+	const urlParams = new URLSearchParams(window.location.search);
+	const productId = urlParams.get("id");
+
 	// Fetch the product data from the database Once we have a db
 	// Temporary data here for testing
 	const data = [
 		{
-			"name": "Windows Key",
+			"id": "1",
+			"name": "Windows 10 Home Key",
 			"price": "$20.00",
-			"image": "windowsKey.jpg",
+			"salePrice": "",
+			"image": "winKeyImg.jpg",
 			"tags": ["microsoft product", "operating system"]
 		},
 		{
-			"name": "Baldurs Gate Key",
+			"id": "2",
+			"name": "Baldurs Gate 3 (PC) Key",
 			"price": "$34.00",
-			"image": "bgKey.jpg",
+			"salePrice": "",
+			"image": "bg3KeyImg.jpg",
 			"tags": ["game", "rpg", "open world"]
 			
 		},
 		{
+			"id": "3",
 			"name": "Starfield (PC) Key",
-			"price": "$34.00",
-			"image": "bgKey.jpg",
+			"price": "$38.00",
+			"salePrice": "$29.99",
+			"image": "stfKeyImg.jpg",
 			"tags": ["game", "rpg", "open world", "pc", "sale"]
 			
 		},
 	];
+
 	// Temporary loading of data till we have DB
 	data.forEach((product) => {
 		const card = createProductCard(product);
 		productContainer.appendChild(card);
-	})
+		})
+	
 	
 	// Function to add product cards to the page 
 	function createProductCard(product) {
 		// Create a new element for the product card
 		const cardDiv = document.createElement("div");
 		cardDiv.className = "col mb-5"
+
+
 		
+		// Check if there is a sale
+		const hasSalePrice = product.salePrice !== "";
+
+		// Calculate the sale percentage
+		let salePercentage = "";
+		if (hasSalePrice) {
+			const regularPrice = parseFloat(product.price.replace("$", "").split(" - ")[0]);
+			const salePrice = parseFloat(product.salePrice.replace("$", ""));
+			const discount = regularPrice - salePrice;
+			salePercentage = `${((discount / regularPrice) * 100).toFixed(0)}% Off!`;
+		}
 		// Populate the card with product data
 		cardDiv.innerHTML = `
 			<div class="card h-100">
-				<img class="card-img-top" src="images/${product.image})" alt="Product Image" />
+				<img class="card-img-top" src="assets/${product.image}" alt="Product Image" />
 				<div class="card-body p-4">
 					<div class="text-center">
 						<h5 class="fw-bolder">${product.name}</h5>
-						<p>${product.price}</p>
+						<p>
+                        	${hasSalePrice
+                            	? `<del>${product.price}</del> ${product.salePrice}`
+                            	: product.price}
+                    	</p>
+						${hasSalePrice ? `<p class="text-danger">${salePercentage}</p>` : ""}
 					</div>
 				</div>
 				<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
 					<div class="text-center">
-						<a class="btn btn-outline-dark mt-auto" href="#">View Product</a>
+						<a class="btn btn-outline-dark mt-auto" href="html/product.html?id=${product.id}">View Product</a>
 					</div>
 				</div>
 			</div>
@@ -77,4 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		});
 	});
+
+
+
 });
